729dbd102f11ff37d0050ffc9d07dafe4375929a
biraj phuyal <birajphuyal@birajs-MacBook-Pro.local>
2025-04-15 15:04:39 +0100

    Just messing with file descritor

---
+++ get_next_line.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd)
+{
+    char *str;
+    int buff;
+    char *read_line;
+
+    buff = 600;
+    str = malloc(sizeof(char) * (buff));
+    while(read(fd, str, buff))
+    {
+        read_line = str;
+        if (str == NULL)
+            return (NULL);
+        if (read_line == '\n')
+        {
+            read_line = strcat(read_line, "\n");
+            return (read_line);
+        }
+    }
+    free(str);
+}
